7f1ed82a2ad05377b79d4f4061d1885db26aca84
Nayon <nayonroy64@gmail.com>
2022-11-24 00:57:09 +0600

    start develop site and install react-router-dom ,tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        bookAndCo: {
+          primary: "#1D3151",
+          secondary: "#4476AC",
+          accent: "#E06A4F",
+          neutral: "#EA9081",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
