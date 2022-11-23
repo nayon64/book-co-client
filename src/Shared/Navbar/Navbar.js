be4cc204bcd70f87362d0a6728eb062a203a7535
Nayon <nayonroy64@gmail.com>
2022-11-24 01:18:10 +0600

    add navbar

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,11 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "../../assets/image/logo.png"
 
 const Navbar = () => {
+
+
+	// navbar menu item 
+	const menuItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+
+      <li>
+        <Link to="/">Blogs</Link>
+      </li>
+      <li>
+        <Link to="/">Dashboard</Link>
+      </li>
+    </>
+  );
+
 	return (
-		<div>
-			<h1>This is navbar</h1>
-		</div>
-	);
+    <div>
+      <div className="navbar max-w-7xl mx-auto py-2">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <Link to="/" className='h-20'>
+            <img className="h-full" src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link to="/" className="btn btn-secondary btn-md text-white">
+           LogIn
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
