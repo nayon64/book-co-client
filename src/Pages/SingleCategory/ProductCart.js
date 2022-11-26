1beebca3bd059c42c6cd38a91ae00f44a5dc7c87
Nayon <nayonroy64@gmail.com>
2022-11-26 19:30:30 +0600

    add single product cart

--- src/Pages/SingleCategory/ProductCart.js
+++ src/Pages/SingleCategory/ProductCart.js
@@ -1,12 +1,29 @@
 import { format } from 'date-fns';
 import React from 'react';
+import toast from 'react-hot-toast';
 import { FaClock, FaMapMarkerAlt, FaUserEdit } from 'react-icons/fa';
 
-const ProductCart = ({ product }) => {
-	const { bookName, bookImg,sellerName, bookDescription, publishDate ,sellerLocation, bookUsed, sellingPrice,originalPrice} = product;
+const ProductCart = ({ product,handleProductBooking ,bookingLoading}) => {
+	const { bookName, bookImg,sellerName, bookDescription, publishDate ,sellerLocation, bookUsed, sellingPrice,originalPrice,_id} = product;
 	const date = new Date(publishDate);
 	console.log(date)
-	const pdate= format(date,"pp PP")
+	const pdate = format(date, "pp PP")
+	
+	const handleProductReport = (id) => {
+		console.log("click",id)
+		fetch(`http://localhost:5000/bookReported/${id}`, {
+			method:"PUT"
+		})
+			.then(res => res.json())
+			.then(data => {
+				if (data.modifiedCount > 0) {
+				toast.success("Reported Success fully")
+			};
+		})
+	}
+	
+
+
 	return (
     <div className="card bg-base-100 shadow-xl">
       <figure className="px-5 pt-10">
@@ -18,9 +35,18 @@ const ProductCart = ({ product }) => {
           <h5 className="font-semibold text-primary text-xl flex items-center">
             <FaUserEdit className="text-secondary mr-1 " /> {sellerName}
           </h5>
-          <h5 className="font-semibold text-primary  text-lg cursor-pointer">
-            Report Product
-          </h5>
+          {product?.isReported ? (
+            <h5 className="font-semibold text-accent  text-lg cursor-pointer">
+              Product Reported
+            </h5>
+          ) : (
+            <h5
+              onClick={() => handleProductReport(_id)}
+              className="font-semibold text-primary  text-lg cursor-pointer"
+            >
+              Report Product
+            </h5>
+          )}
         </div>
         <p className="text-base flex items-center">
           <FaClock />
@@ -41,12 +67,17 @@ const ProductCart = ({ product }) => {
             <span className="text-base font-bold">Used : </span> {bookUsed}
           </p>
         </div>
-        <div className='flex justify-between items-center'>
+        <div className="flex justify-between items-center">
           <div className="font-semibold text-2xl text-accent">
             <span className="text-3xl font-bold">${sellingPrice}</span>
             <span className="line-through ml-2">${originalPrice}</span>
-					</div>
-					<button className='px-2 py-2 bg-primary rounded text-white hover:bg-secondary duration-500'>Book Now</button>
+          </div>
+          <button
+            onClick={() => handleProductBooking(_id)}
+            className={`px-2 py-2 bg-primary rounded text-white hover:bg-secondary duration-500 ${bookingLoading && "disabled"}`} 
+          >
+            Book Now
+          </button>
         </div>
       </div>
     </div>
--- src/Pages/SingleCategory/SingleCategory.js
+++ src/Pages/SingleCategory/SingleCategory.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { useLoaderData } from 'react-router-dom';
 import ProductCart from './ProductCart';
 
 const SingleCategory = () => {
 	const products = useLoaderData()
+	const [bookingLoading,setBookingLoading]=useState(false)
+
+	const handleProductBooking = (id) => {
+		console.log("click", id);
+		setBookingLoading(true)
+		
+    fetch(`http://localhost:5000/bookBooking/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+			toast.success("Book Booking Successfull");
+			setBookingLoading(false)
+        }
+      });
+  };
 	console.log(products)
 	return (
     <div className="max-w-7xl mx-auto">
-      <div className='grid md:grid-cols-2 lg:grid-cols-3  gap-2'>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3  gap-2">
         {products && (
           <div>
             {products.map((product) => (
-              <ProductCart key={product._id} product={product}></ProductCart>
+              <ProductCart
+                key={product._id}
+                product={product}
+                handleProductBooking={handleProductBooking}
+                bookingLoading={bookingLoading}
+              ></ProductCart>
             ))}
           </div>
         )}
