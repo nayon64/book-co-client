b0f658efadad6500d7573ea704837eac31950c78
Nayon <nayonroy64@gmail.com>
2023-01-29 01:33:28 +0600

    add feedback section

--- src/Pages/Home/Feedback/Feedback.js
+++ src/Pages/Home/Feedback/Feedback.js
@@ -1,32 +1,62 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Loader from '../../../conponents/Loader/Loader';
-import FeedbackCard from "./FeedbackCard"
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { Link } from "react-router-dom";
+import Loader from "../../../conponents/Loader/Loader";
+import FeedbackCard from "./FeedbackCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Pagination, Autoplay } from "swiper";
 
 const Feedback = () => {
-	const { data: feedbacks = [], isLoading } = useQuery({
-		queryKey: ["feedbacks"],
-		queryFn: async () => {
-			const res = await fetch(`https://book-co-server.vercel.app/feedbacks`);
-			const data = await res.json()
-			return data.slice(0,3)
-		}
-	})
-	console.log(feedbacks)
+  const { data: feedbacks = [], isLoading } = useQuery({
+    queryKey: ["feedbacks"],
+    queryFn: async () => {
+      const res = await fetch(`https://book-co-server.vercel.app/feedbacks`);
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log("feedbacks", feedbacks);
 
-	if (isLoading) {
-		return <Loader></Loader>
-	}
+  if (isLoading) {
+    return <Loader></Loader>;
+  }
 
-	return (
+  return (
     <section className="md:mt-16 mt-6">
       <h1 className="text-center text-primary font-bold text-4xl divider">
         Feedbacks
       </h1>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 p-4 gap-4 mt-10">
-        {feedbacks.map((category,i) => <FeedbackCard key={i} feedback={category}></FeedbackCard>  
-        )}
+      <div className="md:h-[80vh] h-80 my-12  pr-8  md:pr-16 bg-contain  relative rounded-2xl overflow-hidden flex items-center">
+        <Swiper
+          direction={"vertical"}
+          pagination={true}
+          loop={true}
+          autoplay={{
+            delay: 6000,
+          }}
+          modules={[Pagination, Autoplay]}
+          className="h-5/6 bg-emerald-700 rounded-xl overflow-hidden"
+        >
+          {feedbacks.map((feedback) => (
+            <SwiperSlide key={feedback._id}>
+              <FeedbackCard feedback={feedback} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div
+          className="bg-cover w-1/2  h-full absolute bg-no-repeat top-0 right-0"
+          style={{
+            backgroundImage: `url(https://images.unsplash.com/photo-1513001900722-370f803f498d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80)`,
+          }}
+        ></div>
       </div>
     </section>
   );
--- src/Pages/Home/Feedback/FeedbackCard.js
+++ src/Pages/Home/Feedback/FeedbackCard.js
@@ -3,20 +3,19 @@ import React from 'react';
 const FeddbackCard = ({ feedback }) => {
 	console.log(feedback)
 	return (
-    <div className="border h-full shadow-lg rounded-lg p-5 flex">
-      <div>
-        <div className="avatar">
-          <div className="w-12 rounded-full">
-            <img src={feedback?.img} alt="" />
-          </div>
-        </div>
+    <div className=" flex">
+      <div className="w-2/6 h-full object-cover">
+        <img className="w-full  " src={feedback?.img} alt="" />
       </div>
-      <div className='ml-3'>
-        <div>
-          <h4 className='text-xl font-semibold'>{feedback?.name}</h4>
-        </div>
-        <div className='mt-1'>
-          <p className='text-sm text-gray-600'>{feedback?.message.slice(0,120)}...</p>
+      <div className="ml-3 p-6  w-4/6 -mt-12 pr-12 ">
+        <div className='flex flex-col justify-center  h-full'>
+          <h2 className="text-4xl font-bold text-white border-l-8 pl-8 py-4 border-red-500">
+            {feedback?.title}
+          </h2>
+          <p className='pl-12 py-8 text-gray-300 text-xl font-sm'>{feedback?.message}</p>
+          <div>
+            <h3 className='ml-12 text-white text-3xl font-bold'>{feedback?.userName}</h3>
+          </div>
         </div>
       </div>
     </div>
